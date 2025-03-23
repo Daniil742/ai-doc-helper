195007e009009836e9318a3c8617cac69062f4c0
Daniil742 <55189400+Daniil742@users.noreply.github.com>
2025-03-23 11:54:16 +0400

    U

--- script.js
+++ script.js
@@ -72,7 +72,8 @@ async function sendDirectChatRequest(finalPrompt, spinnerIndicator) {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				"Authorization": `Bearer ${apiKey}`
+				"Authorization": `Bearer ${apiKey}`,
+				//"Access-Control-Allow-Origin": "https://daniil742.github.io"
 			},
 			body: JSON.stringify(payload)
 		});
